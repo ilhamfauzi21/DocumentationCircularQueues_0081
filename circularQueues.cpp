ab65442872515a509111316af277323c2027fe99
ilhamfauzi21 <ilham.fauzi.ft21@mail.umy.ac.id>
2025-06-24 15:33:53 +0700

    DocumentationCircularQueues_081

---
+++ circularQueues.cpp
@@ -0,0 +1,232 @@
+/**
+ * @mainpage Documentation Circular Queues
+ * 
+ * @section Introduction 
+ * Project int merupakan project struktur data 
+ * menggunakan struktur data queues dengan mendekatkan circular arrays.
+ * 
+ * @section Operations
+ * Project int memiliki beberapa operasi antara lain :
+ * 1. Insert
+ * 2. Delete
+ * 3. Display 
+ * 
+ * @section Cara penggunaan 
+ * Berikut beberapa menu yang bisa di gunakan :
+ * 1. en queue
+ * 2. de queue
+ * 3. display
+ * 4. exit
+ * 
+ * @author Profil
+ * - nama :  Ilham Fauzi 
+ * - nim :   20210140081 
+ * - kelas :  B 
+ 
+ * @brief
+ * @version 1:0
+ * @date 2025-06-04
+ * 
+ * @copyright ilham.fauzi.ft21@mail.umy.ac.id (c) 2025
+ * 
+ */
+
+ #include <iostream>
+using namespace std;
+/**
+ * @class Queues
+ * @brief class ini untuk operasi leangkap queues
+ *
+ *
+ *
+ */
+class Queues
+{
+private:
+    int FRONT;          ///< variable private front untuk menyimpan posisi depan antrian
+    int REAR;           ///< variable private rear untuk menyimopan posisi belakang antrian
+    int max = 5;        ///< variable private max untuk menyimpan ukuran maximum antrian
+    int queue_array[5]; ///< variable private queue_array untuk menyimpan elemen antrian
+
+public:
+    /**
+     * @brief Construct a new Queues object
+     * set default queues null
+     * with front = -1 and rear = -1
+     */
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+
+    /**
+     * @brief method untuk memasukkan data dalam antrian
+     * data dimasukkan dalam variable queue_array
+     */
+void insert()
+    {
+        int num; ///< variable num untuk menyimpan nilai
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        // Cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueues overflow\n";
+            return;
+        }
+
+        // cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
+            // jika REAR berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+    /**
+     * @brief method untuk menghapus data dalam antrian
+     * data dihapuskan dari dalam variable queue_array
+     */
+    void remove()
+    {
+        // Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue underflow\n";
+            return;
+        }
+        cout << "\nThe element deleted from the queus is: " << queue_array[REAR];
+
+        // Cek jika antrian hanya memiliki satu alemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            // jika element yang di hapus berada diposisi terakhir array, kembali
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
+    /**
+     * @brief method untuk menampilkan data dalam antrian
+     * data ditampilkan yang berada variable queue_array
+     */
+    void display()
+    {
+        int FRONT_position = FRONT; ///< variable front_position untuk menandakan posisi element pertama pada variable front
+        int REAR_position = REAR;   ///< variable rear_position untuk menandakan posisi element terakhir pada variable rear
+
+        // Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queus is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the queus are...\n";
+        // jika FRONT_position <= REAR_position iterasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+
+        else
+        {
+            // jika FRON_position > REAR_position, iterasi dari FRONT hingga akhir
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
+
+            // Iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
+};
+
+/**
+ * @brief method utama menjelaskan  program
+ *
+ * @return int
+ */
+int main()
+{
+    Queues q; ///< object untuk menggunakan member yang ada pada class Queues
+    char ch;  ///< variable ch untuk menyimpan pilihan pada menu yang diberikan
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement delete operation" << endl;
+            cout << "3. Display Value" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid option" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "Check for the values entered." << endl;
+        }
+    }
+}
